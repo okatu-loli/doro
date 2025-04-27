9a97aded96a7d2e6acd861dcc058d43a6cf81147
quyansiyuanwang <2301179475@qq.com>
2025-04-27 18:48:39 +0800

    chore & test: test:     - 更新`config_test.py`适配新数据结构
    chore:
        - 删除`config.json`的用户配置文件
        - 更新`.gitignore`忽略`config.json`

--- Test/config_test.py
+++ Test/config_test.py
@@ -1,19 +1,41 @@
 import unittest
 
 from src.config import Config
+import os
 
 
 class TestConfig(unittest.TestCase):
     def setUp(self):
-        self.config = Config()
+        # reset config file
+        config_path = "resources/config/config.json"
+        if os.path.exists(config_path):
+            os.remove(config_path)
+        self.config: Config = Config()
 
     def test_default_values(self):
-        """Test default values in the config"
+        """Test default values in the config
         This test only check the default values of the config file.
         """
-        self.assertEqual(self.config.window_width, 200)
-        self.assertEqual(self.config.window_height, 200)
-        self.assertEqual(self.config.animation_fps, 30)
-        self.assertTrue(self.config.show_info)
-        self.assertEqual(self.config.current_theme, "粉色主题")
-        self.assertTrue(self.config.allow_random_movement is True)
+        self.assertEqual(self.config.config["WINDOW"]["WINDOW_WIDTH"], 200)
+        self.assertEqual(self.config.config["WINDOW"]["WINDOW_HEIGHT"], 200)
+        self.assertEqual(self.config.config["ANIMATION"]["ANIMATION_FPS"], 30)
+        self.assertTrue(self.config.config["INFO"]["SHOW_INFO"] is True)
+        self.assertEqual(self.config.config["THEME"]["DEFAULT_THEME"], "粉色主题")
+        self.assertTrue(
+            self.config.config["WORKSPACE"]["ALLOW_RANDOM_MOVEMENT"] is True
+        )
+
+    def test_modify_config(self):
+        """Test modify config"""
+        self.config.config["WINDOW"]["WINDOW_WIDTH"] = 300
+        self.config.config["WINDOW"]["WINDOW_HEIGHT"] = 300
+        self.config.save()
+        new_config = Config()
+        self.assertEqual(new_config.config["WINDOW"]["WINDOW_WIDTH"], 300)
+        self.assertEqual(new_config.config["WINDOW"]["WINDOW_HEIGHT"], 300)
+
+    def tearDown(self) -> None:
+        # delete config file
+        config_path = "resources/config/config.json"
+        if os.path.exists(config_path):
+            os.remove(config_path)
