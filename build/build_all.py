3919bc272779d4870a3e385bceb42a7a553f1468
quyansiyuanwang <2301179475@qq.com>
2025-05-07 00:54:44 +0800

    feat: 增加了 build-dev.py 脚本, 这是一个用于构建与规范开发环境的脚本。
    使用命令 python build-dev.py --help 可以查看所有可用的命令和选项。
    可使用命令 python build-dev.py [command] 来执行不同的构建任务。

--- build/build_all.py
+++ build/build_all.py
@@ -3,28 +3,34 @@ import subprocess
 import re
 from typing import List, Tuple
 
-current_dir: str = os.path.dirname(os.path.abspath(__file__))
 
-python_files = [
-    f
-    for f in os.listdir(current_dir)
-    if f.endswith(".py") and f != os.path.basename(__file__)
-]
+def main():
+    current_dir: str = os.path.dirname(os.path.abspath(__file__))
 
-execution_order: List[Tuple[int, str]] = []
+    python_files = [
+        f
+        for f in os.listdir(current_dir)
+        if f.endswith(".py") and f != os.path.basename(__file__)
+    ]
 
-for py_file in python_files:
-    file_path = os.path.join(current_dir, py_file)
-    with open(file_path, "r", encoding="utf-8") as f:
-        content = f.read()
-        match = re.search(r"\[#ExecutionOrder\s*=\s*(\d+)\]", content)
-        if match:
-            order = int(match.group(1))
-            execution_order.append((order, py_file))
+    execution_order: List[Tuple[int, str]] = []
 
-execution_order.sort(key=lambda x: x[0])
+    for py_file in python_files:
+        file_path = os.path.join(current_dir, py_file)
+        with open(file_path, "r", encoding="utf-8") as f:
+            content = f.read()
+            match = re.search(r"\[#ExecutionOrder\s*=\s*(\d+)\]", content)
+            if match:
+                order = int(match.group(1))
+                execution_order.append((order, py_file))
 
-for order, py_file in execution_order:
-    file_path = os.path.join(current_dir, py_file)
-    print(f"Order: {order}, Running {py_file}...")
-    subprocess.run(["python", file_path])
+    execution_order.sort(key=lambda x: x[0])
+
+    for order, py_file in execution_order:
+        file_path = os.path.join(current_dir, py_file)
+        print(f"Order: {order}, Running {py_file}...")
+        subprocess.run(["python", file_path])
+
+
+if __name__ == "__main__":
+    main()
