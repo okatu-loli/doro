620b4d1f14a59e692ebfa1c9f59e207e86685eeb
quyansiyuanwang <2301179475@qq.com>
2025-05-07 01:13:18 +0800

    fix: 更新了config_test.py中的对config的测试接口

--- Test/config_test.py
+++ Test/config_test.py
@@ -10,29 +10,30 @@ class TestConfig(unittest.TestCase):
         config_path = "resources/config/config.json"
         if os.path.exists(config_path):
             os.remove(config_path)
+
         self.config: Config = Config()
+        self.config.save()
+        self.config = Config()
 
     def test_default_values(self):
         """Test default values in the config
         This test only check the default values of the config file.
         """
-        self.assertEqual(self.config.config["WINDOW"]["WINDOW_WIDTH"], 200)
-        self.assertEqual(self.config.config["WINDOW"]["WINDOW_HEIGHT"], 200)
-        self.assertEqual(self.config.config["ANIMATION"]["ANIMATION_FPS"], 30)
-        self.assertTrue(self.config.config["INFO"]["SHOW_INFO"] is True)
-        self.assertEqual(self.config.config["THEME"]["DEFAULT_THEME"], "粉色主题")
-        self.assertTrue(
-            self.config.config["WORKSPACE"]["ALLOW_RANDOM_MOVEMENT"] is True
-        )
+        self.assertEqual(self.config.config["Window"]["Width"], 200)
+        self.assertEqual(self.config.config["Window"]["Height"], 200)
+        self.assertEqual(self.config.config["Animation"]["FPS"], 30)
+        self.assertTrue(self.config.config["Info"]["ShowInfo"] is True)
+        self.assertEqual(self.config.config["Theme"]["DefaultTheme"], "粉色主题")
+        self.assertTrue(self.config.config["Workspace"]["AllowRandomMovement"] is True)
 
     def test_modify_config(self):
         """Test modify config"""
-        self.config.config["WINDOW"]["WINDOW_WIDTH"] = 300
-        self.config.config["WINDOW"]["WINDOW_HEIGHT"] = 300
+        self.config.config["Window"]["Width"] = 300
+        self.config.config["Window"]["Height"] = 300
         self.config.save()
         new_config = Config()
-        self.assertEqual(new_config.config["WINDOW"]["WINDOW_WIDTH"], 300)
-        self.assertEqual(new_config.config["WINDOW"]["WINDOW_HEIGHT"], 300)
+        self.assertEqual(new_config.config["Window"]["Width"], 300)
+        self.assertEqual(new_config.config["Window"]["Height"], 300)
 
     def tearDown(self) -> None:
         # delete config file
